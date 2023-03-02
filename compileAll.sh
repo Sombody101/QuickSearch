87697b88218a0a74388820d4cf0dd3ed38f152fb
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2023-03-02 09:46:41 -0700

    v1.0.5.6.3
    Introduced framework dependent binaries for less storage space and faster execution.

--- compileAll.sh
+++ compileAll.sh
@@ -13,11 +13,22 @@ targets=(
     "win-arm64"
 )
 
-targetPath="$HOME/cs/qs/PrecompiledBinaries"
+targetPath="$HOME/cs/qs/PrecompiledBinaries/PackedWithRuntime"
 
+echo Starting runtime-packed builds
 [[ ! -d "$targetPath" ]] && mkdir "$targetPath"
 
 for target in "${targets[@]}"; do
     dotnet publish -r "$target" -p:PublishSingleFile=true --self-contained true --output "$targetPath" -p:AssemblyName="qs-$target"
+    [[ -f "$targetPath/qs-$target.pdb" ]] && rm "$targetPath/qs-$target.pdb"
+done
+
+targetPath="$HOME/cs/qs/PrecompiledBinaries/DependantOnRuntime"
+
+echo Starting dependant builds
+[[ ! -d "$targetPath" ]] && mkdir "$targetPath"
+
+for target in "${targets[@]}"; do
+    dotnet publish -r "$target" -p:PublishSingleFile=true --self-contained false --output "$targetPath" -p:AssemblyName="qs-$target"
     [[ -f "$targetPath/qs-$target.pdb" ]] && rm "$targetPath/qs-$target.pdb"
 done
